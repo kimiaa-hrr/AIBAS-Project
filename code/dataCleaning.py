801e04c6bb092f66cb049be630c125400b1845e0
user <user@kimia.local>
2026-01-25 09:07:36 +0100

    added code for one-hot encoding

--- code/dataCleaning.py
+++ code/dataCleaning.py
@@ -17,6 +17,12 @@ df = df.dropna(subset=['Sport_Type'])
 df = df.dropna(subset=['Event'])
 
 
+# Only replace actual missing values, not the string "None"
+df["Injury_History"] = df["Injury_History"].fillna("None")
+
+# Clean up whitespace/capitalization
+df["Injury_History"] = df["Injury_History"].str.strip().str.capitalize()
+
 
 
 # Fill numeric columns with median
@@ -29,11 +35,14 @@ for col in cat_cols:
     df[col] = df[col].fillna(df[col].mode()[0])
 
 
+
+
+
 # the Athlete_id seems to have duplicates so I added a new column for Ids so it is unique
 df['Athlete_ID_New'] = ['A{:03d}'.format(i+1) for i in range(len(df))]
-print(df)
-
-
+# Remove the column from its current position and insert at index 0
+col = df.pop('Athlete_ID_New')
+df.insert(0, 'Athlete_ID_New', col)
 
 
 
@@ -59,6 +68,74 @@ print(f"Number of rows after removing outliers: {len(df)}")
 scaler = MinMaxScaler()
 df[numeric_cols] = scaler.fit_transform(df[numeric_cols])
 
+
+
+
+# ============== Data Encoding ==============
+
+# Check unique values
+print(df["Injury_History"].unique())
+
+#clean whitespace/capitalization
+df["Injury_History"] = df["Injury_History"].str.strip().str.capitalize()
+
+# Convert to Categorical with ordered categories
+df["Injury_History"] = pd.Categorical(
+    df["Injury_History"],
+    categories=["None", "Minor", "Major"],
+    ordered=True
+)
+
+
+
+
+# Check unique values
+print(df["Sport_Type"].unique())
+
+#clean whitespace/capitalization
+df["Sport_Type"] = df["Sport_Type"].str.strip().str.capitalize()
+
+# Convert to Categorical with ordered categories
+df["Sport_Type"] = pd.Categorical(
+    df["Sport_Type"],
+    categories=['Running', 'Swimming', 'Cycling', 'Soccer', 'Basketball', 'Tennis'],
+    ordered=True
+)
+
+
+
+# Check unique values
+print(df["Training_Intensity"].unique())
+
+#clean whitespace/capitalization
+df["Training_Intensity"] = df["Training_Intensity"].str.strip().str.capitalize()
+
+# Convert to Categorical with ordered categories
+df["Training_Intensity"] = pd.Categorical(
+    df["Training_Intensity"],
+    categories=['Low', 'High', 'Medium'],
+    ordered=True
+)
+
+
+
+
+# Check unique values
+print(df["Altitude_Training"].unique())
+
+#clean whitespace/capitalization
+df["Altitude_Training"] = df["Altitude_Training"].str.strip().str.capitalize()
+
+# Convert to Categorical with ordered categories
+df["Altitude_Training"] = pd.Categorical(
+    df["Altitude_Training"],
+    categories=['Low', 'High', 'Medium'],
+    ordered=True
+)
+
+
+df_encoded = pd.get_dummies(df, columns=["Injury_History","Sport_Type","Training_Intensity","Altitude_Training"], drop_first=True)
+df = df_encoded 
 
 
 
